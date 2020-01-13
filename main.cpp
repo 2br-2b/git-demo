7c57e1badfedac68954aea888f245f3674b58dcb
Kevin Roche <kroche.md@gmail.com>
2020-01-13 17:48:02 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+	int a, b, r;
+	char choice = ' ';
+	while (true) {
+		std::cout << "Enter 'a' for addition or 'm' for multiplication: ";
+		std::cin >> choice;
+		if (choice == 'a' || choice == 'A' || choice == 'm' || choice == 'M')
+			break;
+		else
+			std::cout << "Invalid choice." << std::endl;
+	}
+	std::cout << "Enter the first number: ";
+	std::cin >> a;
+	std::cout << "Enter the second number: ";
+	std::cin >> b;
+	switch(choice) {
+		case 'a':
+		case 'A':
+			r = a + b;
+			break;
+		case 'm':
+		case 'M':
+			r = a * b;
+			break;
+	}
+	std::cout << "The result is " << r << std::endl;
+	return 0;
+}
